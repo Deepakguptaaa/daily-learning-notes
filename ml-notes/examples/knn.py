b35895075de3e8bcf43d62ee6077849cb98e9dcb
Deepak Gupta <dg3765856@gmail.com>
2026-06-14 22:21:10 +0530

    feat: add KNN example

---
+++ ml-notes/examples/knn.py
@@ -0,0 +1,11 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+X = [[1], [2], [3], [6], [7], [8]]
+y = [0, 0, 0, 1, 1, 1]
+
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X, y)
+
+prediction = model.predict([[5]])
+
+print(prediction)
